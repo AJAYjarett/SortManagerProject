5d630fe55a102a14331fe66aba0dd017f86d1df2
Alexander Jarrett <alexjarrett400@gmail.com>
2021-12-13 15:52:56 +0000

    Adding repeating functionality and ending display

--- src/main/java/app/RuntimeController.java
+++ src/main/java/app/RuntimeController.java
@@ -10,14 +10,20 @@ import sorters.SorterRunning;
 public class RuntimeController {
 
     public static void runSortManager(){
+        boolean continueSorting = true;
         int[] arrayToSort;
         int[] arraySorted;
-        OutputMaker.displayMenu();
-        Sorter sorter = SorterFactory.getSorter(InputReader.getInput());
-        OutputMaker.displayUpperBoundIn();
-        arrayToSort = RandomArray.getRandomArray(Integer.parseInt(InputReader.getInput()));
-        OutputMaker.displayRandomArray(arrayToSort);
-        arraySorted = SorterRunning.runSorter(sorter,arrayToSort);
-        OutputMaker.displaySortedArray(arraySorted);
+        while (continueSorting){
+            OutputMaker.displayMenu();
+            Sorter sorter = SorterFactory.getSorter(InputReader.getInput());
+            OutputMaker.displayUpperBoundIn();
+            arrayToSort = RandomArray.getRandomArray(Integer.parseInt(InputReader.getInput()));
+            OutputMaker.displayRandomArray(arrayToSort);
+            arraySorted = SorterRunning.runSorter(sorter,arrayToSort);
+            OutputMaker.displaySortedArray(arraySorted);
+            OutputMaker.displayEnding();
+            if (InputReader.getInput().equals("1")) continueSorting = false;
+        }
+        InputReader.closeScanner();
     }
 }
