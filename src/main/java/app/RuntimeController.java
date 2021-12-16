46db8d19cd224e40aa4f629c3227685603cd0796
Alexander Jarrett <alexjarrett400@gmail.com>
2021-12-16 11:11:30 +0000

    Split RuntimeController down into RuntimeTasks

--- src/main/java/app/RuntimeController.java
+++ src/main/java/app/RuntimeController.java
@@ -1,30 +1,18 @@
 package app;
 
 import input_output.InputReader;
-import input_output.OutputMaker;
-import random_array.RandomArray;
 import sorters.Sorter;
-import sorters.SorterFactory;
-import sorters.SorterRunner;
 
 public class RuntimeController {
 
     public static void runSortManager(){
         boolean continueSorting = true;
         int[] arrayToSort;
-        int[] arraySorted;
         while (continueSorting){
-            OutputMaker.displayMenu();
-            Sorter sorter = SorterFactory.getSorter(InputReader.getInput());
-            OutputMaker.displayUpperBoundIn();
-            arrayToSort = RandomArray.getRandomArray(Integer.parseInt(InputReader.getInput()));
-            OutputMaker.displayRandomArray(arrayToSort);
-            SorterRunner sorterRunner = new SorterRunner(sorter,arrayToSort);
-            arraySorted = sorterRunner.getSortedArr();
-            OutputMaker.displaySortedArray(arraySorted);
-            OutputMaker.displayTimeTaken(sorterRunner.getTimeTaken());
-            OutputMaker.displayEnding();
-            if (InputReader.getInput().equals("1")) continueSorting = false;
+            Sorter sorter = RuntimeTasks.getSorter();
+            arrayToSort = RuntimeTasks.getAndShowRandomArray();
+            RuntimeTasks.sortAndShowArray(sorter, arrayToSort);
+            continueSorting = RuntimeTasks.endOrContinue();
         }
         InputReader.closeScanner();
     }
