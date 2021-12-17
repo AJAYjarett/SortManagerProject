d4dfc123c331653b4eb8dd3e2e296bad03a6354a
Alexander Jarrett <alexjarrett400@gmail.com>
2021-12-17 17:50:45 +0000

    Added logging to mylog.log in logger package added logging messages elsewhere

--- src/main/java/app/RuntimeController.java
+++ src/main/java/app/RuntimeController.java
@@ -1,17 +1,23 @@
 package app;
 
 import input_output.InputReader;
+import logger.LogWriter;
 import sorters.Sorter;
+
+import java.util.logging.Level;
 
 public class RuntimeController {
 
     //Deals with the overall running of the code, the highest level controller
     public static void runSortManager(){
+        LogWriter.createLogger();
+        LogWriter.writeLog(Level.INFO, "Program begins");
         boolean continueSorting = true;
         int[] arrayToSort;
         while (continueSorting){
 
             //Stage 1. get the right sorter
+
             Sorter sorter = RuntimeTasks.getSorter();
 
             //Stage 2. get a random array and sort it
@@ -22,5 +28,6 @@ public class RuntimeController {
             continueSorting = RuntimeTasks.endOrContinue();
         }
         InputReader.closeScanner();
+        LogWriter.writeLog(Level.INFO, "Ending Program");
     }
 }
--- src/main/java/sorters/SorterRunner.java
+++ src/main/java/sorters/SorterRunner.java
@@ -1,14 +1,21 @@
 package sorters;
 
+import logger.LogWriter;
+
+import java.util.logging.Level;
+
 public class SorterRunner {
 
     private long timeTaken;
     private int[] sortedArr;
 
     public SorterRunner(Sorter sorter, int[] arrToSort) {
+        LogWriter.writeLog(Level.INFO, "Starting sorting timer");
         long startTime = System.nanoTime();
+        LogWriter.writeLog(Level.INFO, "Running Sorter");
         sortedArr = sorter.sort(arrToSort);
         long endTime = System.nanoTime();
+        LogWriter.writeLog(Level.INFO, "Ending sorting timer");
         timeTaken = endTime-startTime;
     }
 
